2e887076e2702227199e4660a183af6f7120ec2e
Rinaldito <rinalditoryanari@gmail.com>
2023-12-23 13:53:20 +0700

    notes for dev

--- controllers/migration/migration.js
+++ controllers/migration/migration.js
@@ -1,58 +1,38 @@
-const validator = require("validator");
-const { User } = require("../../model/users");
+const validator = require('validator');
+const { User } = require('../../model/users');
 const md5 = require('md5');
 
-const {capitalCase} = require('change-case-all');
+const { capitalCase } = require('change-case-all');
 
 const sequelize = require('../../infrastructure/db/sequelize');
-const initModels = require('../../model/initmodels')
-const { Op } = require("sequelize");
-const axios = require("axios");
-const {get} = require("axios");
+const initModels = require('../../model/initmodels');
+const { Op } = require('sequelize');
+const axios = require('axios');
+const { get } = require('axios');
 
 const models = initModels(sequelize);
 
-const showMigrationForm = (req,res,next) => {
+const showMigrationForm = (req, res, next) => {
     let message = null;
-    if(req.query.error === 'empty') {
+    if (req.query.error === 'empty') {
         message = 'Username atau password tidak boleh kosong';
-    } else if(req.query.error === 'identify') {
+    } else if (req.query.error === 'identify') {
         message = 'Username atau password salah';
     }
 
     res.render('migration-form', { title: 'Migrasi SSO', error: message });
-}
-
-// Ajax Function to dynamically verify password
-const verifyPassword = async (req, res, next) => {
-    const { password } = req.body;
-
-    if(validator.isEmpty(password)) {
-        return res.json({ status: 'error', message: 'Password tidak boleh kosong' });
-    }
-
-    if(!validator.isStrongPassword(password, {
-        minLength: 10,
-        minLowercase: 1,
-        minUppercase: 1,
-        minNumbers: 1,
-    })) {
-        return res.json({ status: 'error', message: 'Password harus memiliki kriteria: min 10 karakter, 1 huruf besar, 1 huruf kecil, dan 1 simbol/angka' });
-    }
-
-    res.json({ status: 'success', message: 'Password valid' });
-}
+};
 
 // Ajax Function to verify user and password from the old internet database
 
 const checkUser = async (req, res, next) => {
     const { ident, password } = req.body;
 
-    if(!validator.isNumeric(ident)){
+    if (!validator.isNumeric(ident)) {
         return res.redirect(301, '/migration-form?error=empty');
     }
 
-    if(validator.isEmpty(password)) {
+    if (validator.isEmpty(password)) {
         return res.redirect(301, '/migration-form?error=empty');
     }
 
@@ -60,8 +40,8 @@ const checkUser = async (req, res, next) => {
     const old_user = await models.users.findOne({
         where: {
             username: ident,
-            password: md5(password)
-        }
+            password: md5(password),
+        },
     });
 
     if (old_user === null) {
@@ -70,30 +50,68 @@ const checkUser = async (req, res, next) => {
 
     old_user.nama = capitalCase(old_user.nama);
 
-    res.render('verify', { title: 'Migrasi SSO', old_user: old_user, old_password: password});
-}
+    res.render('verify', { title: 'Migrasi SSO', old_user: old_user, old_password: password });
+};
 
 const migrateUser = async (req, res, next) => {
     const { hid_ident, hid_password } = req.body;
 
+    /*
+    ambil input dari req.body baru 
+    validasi server side
+    - copy function verify yang ada di client side - di verify.hbs
+        dibikin modelan kayak function show migrationForm pake if else dkk
+        error nya nanti dikasih message
+
+    - abis itu kalo kgk ke veirfy langsung di trhow ke /migration
+        return res.redirect(301, '/migration-form?error=empty');
+        error message nya taro di param error
+    */
+
     const old_user = await models.users.findOne({
         where: {
             username: hid_ident,
-            password: md5(hid_password)
-        }
+            password: md5(hid_password),
+        },
     });
 
-    const authenticate = require('../../infrastructure/keycloak/keycloak.js')
+    /*
+        ambil nama sama jurusan, nip/nim dari old
+            sisanya dari form input
+        naamnya pake CapitalCase()
+        
+        Kalo dia mahasiswa -> 
+            harus ngambil 
+                tahun akademik dari api gateaway -> https://apitracer.upatik.io/mhs_tahun_akademik?nim=1907412028
+                {
+                    ...
+                    "th_akademik": 2019
+                }
+                nanti dia masuk ke /mahasiswa/'jurusan'/'th_akademik'
+
+        kalo dosen cuman ambil jurusan doang
+                nanti ke /dosen/jurusan
+
+        kalo staff langsung ke staf/data migrasi
+    */
+
+    const authenticate = require('../../infrastructure/keycloak/keycloak.js');
 
     // TODO: Check data at ACADEMIA / PERSONALIA
     const kcAdminClient = await authenticate;
 
     // TODO: Submit data to Keycloak
-    const kcGroup = await kcAdminClient.groups.find({briefRepresentation: true, search: old_user.jurusan});
-}
+    const kcGroup = await kcAdminClient.groups.find({ briefRepresentation: true, search: old_user.jurusan });
+
+    /*
+    nanti ini bagian lo nyari dkk dan bikin subgroups itu
+    
+    
+    */
+};
 
 module.exports = {
     showMigrationForm,
     checkUser,
-    migrateUser
-}
+    migrateUser,
+};
