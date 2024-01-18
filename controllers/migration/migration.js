b3df5faa618671cac6a80aeb2d90ca30890b22ca
BrilyanSuherman <brilyansuherman@gmail.com>
2024-01-18 10:42:34 +0700

    addToGroupDosen
    add to group dosen

--- controllers/migration/migration.js
+++ controllers/migration/migration.js
@@ -1,160 +1,111 @@
-const validator = require('validator');
-const {User} = require('../../model/users');
-const md5 = require('md5');
+const validator = require("validator");
+const md5 = require("md5");
 
-const {capitalCase} = require('change-case-all');
+const { capitalCase } = require("change-case-all");
 
-const sequelize = require('../../infrastructure/db/sequelize');
-const initModels = require('../../model/initmodels');
-const {Op} = require('sequelize');
-const axios = require('axios');
-const {get} = require('axios');
+const sequelize = require("../../infrastructure/db/sequelize");
+const initModels = require("../../model/initmodels");
+const axios = require("axios");
 const authenticate = require("../../infrastructure/keycloak/keycloak");
-const { application, response } = require('express');
+const { application, response } = require("express");
 
 const models = initModels(sequelize);
 
 const showMigrationForm = (req, res, next) => {
-    let message = null;
-    if (req.query.error === 'empty') {
-        message = 'Username atau password tidak boleh kosong';
-    } else if (req.query.error === 'identify') {
-        message = 'Username atau password salah';
-    }
+	let message = null;
+	if (req.query.error === "empty") {
+		message = "Username atau password tidak boleh kosong";
+	} else if (req.query.error === "identify") {
+		message = "Username atau password salah";
+	}
 
-    res.render('migration-form', {title: 'Migrasi SSO', error: message});
+	res.render("migration-form", { title: "Migrasi SSO", error: message });
 };
 
 // Ajax Function to verify user and password from the old internet database
 
 const checkUser = async (req, res, next) => {
-    const {ident, password} = req.body;
+	const { ident, password } = req.body;
 
-    if (!validator.isNumeric(ident)) {
-        return res.redirect(401, '/migration-form?error=empty');
-    }
+	if (!validator.isNumeric(ident)) {
+		return res.redirect(401, "/migration-form?error=empty");
+	}
 
-    if (validator.isEmpty(password)) {
-        return res.redirect(401, '/migration-form?error=empty');
-    }
+	if (validator.isEmpty(password)) {
+		return res.redirect(401, "/migration-form?error=empty");
+	}
 
-    //Check for existing user and password in the old internet database
-    const old_user = await models.users.findOne({
-        where: {
-            username: ident,
-            password: md5(password),
-        },
-    });
+	//Check for existing user and password in the old internet database
+	const old_user = await models.users.findOne({
+		where: {
+			username: ident,
+			password: md5(password),
+		},
+	});
 
-    if (old_user === null) {
-        return res.redirect(301, '/migration-form?error=identify');
-    }
+	if (old_user === null) {
+		return res.redirect(301, "/migration-form?error=identify");
+	}
 
-    old_user.nama = capitalCase(old_user.nama);
+	old_user.nama = capitalCase(old_user.nama);
 
-    res.render('verify', {title: 'Migrasi SSO', old_user: old_user, old_password: password});
+	res.render("verify", {
+		title: "Migrasi SSO",
+		old_user: old_user,
+		old_password: password,
+	});
 };
 
 const migrateUser = async (req, res, next) => {
-    const {email, password, hid_ident, hid_password} = req.body;
+	const { email, password, hid_ident, hid_password } = req.body;
 
-    let regex = /\b[A-Za-z0-9._%+-]+@(?:[A-Za-z0-9-]+\.)?pnj\.ac\.id\b/;
-    if (!regex.test(email)) {
-        return res.redirect(401, '/migration-form?error=Pastikan mengisi Email dengan benar!');
-    }
+	let regex = /\b[A-Za-z0-9._%+-]+@(?:[A-Za-z0-9-]+\.)?pnj\.ac\.id\b/;
+	if (!regex.test(email)) {
+		return res.redirect(
+			401,
+			"/migration-form?error=Pastikan mengisi Email dengan benar!"
+		);
+	}
 
-    regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}$/;
-    if (!regex.test(password)) {
-        return res.redirect(401, '/migration-form?error=Pastikan mengisi Password dengan benar!');
-    }
+	regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}$/;
+	if (!regex.test(password)) {
+		return res.redirect(
+			401,
+			"/migration-form?error=Pastikan mengisi Password dengan benar!"
+		);
+	}
 
-    const old_user = await models.users.findOne({
-        where: {
-            username: hid_ident,
-            password: md5(hid_password),
-        },
-    });
+	const old_user = await models.users.findOne({
+		where: {
+			username: hid_ident,
+			password: md5(hid_password),
+		},
+	});
 
-    // TODO: Check data at ACADEMIA / PERSONALIA
-    const kcAdminClient = await authenticate;
+	old_user.jabatan = "Dosen";
+	// TODO: Check data at ACADEMIA / PERSONALIA
+	const kcAdminClient = await authenticate;
 
-    // Find The Keycloak Groups
-    const kcGroupJabatan = await kcAdminClient.groups.find({
-        briefRepresentation: true, 
-        search: old_user.jabatan
-    });
+	old_user.jabatan == "Dosen";
 
-    // Split name to firstName and lastName
-    const nameParts = old_user.nama.split(' ');
-    const firstName = nameParts[0];
-    const lastName = nameParts.slice(1).join(' ')
+	// Find The Keycloak Groups
+	let kcGroupJabatan = await kcAdminClient.groups.find({
+		briefRepresentation: true,
+		search: old_user.jabatan,
+	});
+	kcGroupJabatan = kcGroupJabatan.find((group) => group.name == "Dosen");
+	kcGroupJabatan = await kcAdminClient.groups.findOne({
+		briefRepresentation: true,
+		id: kcGroupJabatan.id,
+	});
 
-    if (old_user.jabatan == 'Mahasiswa') {
+	// Split name to firstName and lastName
+	const nameParts = old_user.nama.split(" ");
+	const firstName = nameParts[0];
+	const lastName = nameParts.slice(1).join(" ");
 
-        // create new user object
-        const kcUser = await kcAdminClient.users.create({
-            username: old_user.username,
-            email: email,
-            firstName: firstName,
-            lastName: lastName,
-            emailVerified: true,
-            enabled: true,
-        });
-
-        // ambil subgroup jurusan
-        const kcSubGroupJurusan = kcGroupJabatan[0].subGroups[0];
-
-        // GET informasi mahasiswa from apitracer
-        const apiMahasiswaURL = 'https://apitracer.upatik.io/mhs_tahun_akademik';
-        const responseMahasiswa = await axios.get(apiMahasiswaURL, {
-            params: {
-                nim: old_user.username
-            }
-        });
-
-        // ambil tahun angkatan
-        const tahunAngkatan = responseMahasiswa.data['th_akademik'];
-        // search group angkatan
-        
-        const kcGroupMahasiswa = await kcAdminClient.groups.find({
-            briefRepresentation: true,
-            search: tahunAngkatan,
-        });
-
-        let kcGroupAngkatan = kcGroupMahasiswa[0].subGroups[0].subGroups[0];
-
-        // check angkatan group if existed
-        if (kcGroupAngkatan != null) {
-
-            // add user to group
-            await kcAdminClient.users.addToGroup({
-                id: kcUser.id,
-                groupId: kcGroupAngkatan.id,
-            });
-
-        } else {
-            //  create new angkatan group
-            kcGroupAngkatan = await kcAdminClient.groups.setOrCreateChild(
-                {
-                    id: kcSubGroupJurusan.id,
-                },
-                {
-                    name: tahunAngkatan,
-                    path: kcSubGroupJurusan.path + "/" + tahunAngkatan,
-                }
-            );
-
-            // add user to group
-            await kcAdminClient.users.addToGroup({
-                id: kcUser.id, 
-                groupId: kcGroupAngkatan.id,
-            });
-        }
-
-        res.json(kcGroupAngkatan);
-      
-        /*
-        * array find group mahasiswa -> subgroup jurusan
+	if (old_user.jabatan == "Mahasiswa") {
+		/* array find group mahasiswa -> subgroup jurusan
         * findOne pakai subgroup.id
         *   await kcAdminClient.groups.findOne({briefRepresentation: true, id: kcSubGroup.id})
         * cari tahun akademik pakai api get
@@ -181,12 +132,30 @@ const migrateUser = async (req, res, next) => {
                 lastName: lastName,
             }
         *create user ->  await kcAdminClient.users.create(user)
-        *add user to group -> await kcAdminClient.users.addToGroup({id: user.id, groupId: group Id})
-        * */
-    } else if (old_user.jabatan == 'dosen') {
+        *add user to group -> await kcAdminClient.users.addToGroup({id: user.id, groupId: group Id})*/
+	} else if (old_user.jabatan == "Dosen") {
+		let kcSubGroup = kcGroupJabatan.subGroups.find(
+			(subgroup) => subgroup.name == "Teknik Informatika dan Komputer"
+		);
 
-        /*
-        * array find group group -> subgroup jurusan
+		//Buat user baru
+		const kcUser = await kcAdminClient.users.create({
+			username: old_user.username,
+			email: email,
+			firstName: firstName,
+			lastName: lastName,
+			emailVerified: true,
+			enabled: true,
+		});
+
+		// Tambahkan anggota ke grup
+		const status = await kcAdminClient.users.addToGroup({
+			id: kcUser.id,
+			groupId: kcGroupJabatan.id,
+		});
+
+		res.json(status);
+		/*array find group group -> subgroup jurusan
         * findOne pakai subgroup.id
         *   await kcAdminClient.groups.findOne({briefRepresentation: true, id: kcSubGroup.id})
 
@@ -200,14 +169,11 @@ const migrateUser = async (req, res, next) => {
                 lastName: lastName,
             }
         *create user ->  await kcAdminClient.users.create(user)
-        *add user to group -> await kcAdminClient.users.addToGroup({id: user.id, groupId: group Id})
-        * */
-    } else {
-        //staf
-
-
-        /*
-        * array find group staff -> subgroup jurusan -> cari "Data Migrasi"
+        *add user to group -> await kcAdminClient.users.addToGroup({id: user.id, groupId: group Id}) */
+	} else {
+		//Cari grup 'Staf'
+		//staf
+		/* array find group staff -> subgroup jurusan -> cari "Data Migrasi"
         * findOne pakai subgroup.id
         *   await kcAdminClient.groups.findOne({briefRepresentation: true, id: kcSubGroup.id})
 
@@ -221,17 +187,14 @@ const migrateUser = async (req, res, next) => {
                 lastName: lastName,
             }
         *create user ->  await kcAdminClient.users.create(user)
-        *add user to group -> await kcAdminClient.users.addToGroup({id: user.id, groupId: kcGroupId})
-        * */
-
-    }
-
-    
-    // sampai sini aja dulu
+        *add user to group -> await kcAdminClient.users.addToGroup({id: user.id, groupId: kcGroupId})*/
+	}
 };
 
+// sampai sini aja dulu
+
 module.exports = {
-    showMigrationForm,
-    checkUser,
-    migrateUser,
+	showMigrationForm,
+	checkUser,
+	migrateUser,
 };
